96743ba092be8e3eb443b57716374f05540a4514
sudheerj <sudheer.jonna@gmail.com>
2013-09-15 16:16:53 +0530

    Spring Mapfactory bean

---
+++ SpringBeans/SpringMapFactoryBean/src/main/java/com/sudheer/common/Customer.java
@@ -0,0 +1,23 @@
+package com.sudheer.common;
+
+import java.util.Map;
+
+public class Customer {
+
+	private Map maps;
+
+	public Map getMaps() {
+		return maps;
+	}
+
+	public void setMaps(Map maps) {
+		this.maps = maps;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [maps=" + maps + "]";
+	}
+
+	
+}
