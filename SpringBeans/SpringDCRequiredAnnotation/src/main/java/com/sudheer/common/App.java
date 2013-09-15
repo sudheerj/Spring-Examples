fca1d63219921ed611000fd0945af73c99e16251
sudheerj <sudheer.jonna@gmail.com>
2013-09-15 21:23:35 +0530

    Spring Dependency Checking Required Annotation

---
+++ SpringBeans/SpringDCRequiredAnnotation/src/main/java/com/sudheer/common/App.java
@@ -0,0 +1,22 @@
+package com.sudheer.common;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Spring Properties Dependency Checking
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	ConfigurableApplicationContext context = 
+    	    	  new ClassPathXmlApplicationContext(new String[] {"SpringBeans.xml"});
+    	/*ConfigurableApplicationContext context = 
+  	    	  new ClassPathXmlApplicationContext(new String[] {"SpringBeans-ConstructorInjection.xml"});*/
+    	    	Customer cust = (Customer)context.getBean("CustomerBean");
+    	    	System.out.println(cust);
+    	    	context.close();
+    }
+}
